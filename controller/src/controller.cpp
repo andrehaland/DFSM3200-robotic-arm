0f23c36743a443e89513722d55d44e01d8b80886
andrehaland <andre.haland@gmail.com>
2018-03-26 20:51:37 +0200

    Cleanup in controller

--- controller/src/controller.cpp
+++ controller/src/controller.cpp
@@ -2,12 +2,8 @@
 #include "std_msgs/Float64MultiArray.h"
 
 #include "KinematicHandler.h"
-/**
 
-{
-	ROS_INFO("Hello World [%f]", msg->data[1]);
-}
-**/
+
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "controller");	
@@ -18,15 +14,14 @@ int main(int argc, char** argv)
 	KinematicHandler kinematic(&node_handle);
 
 
-	// Setup subscriber
+	// Setup subscriber to camera to get cartesian coordinates
 	ros::Subscriber camera = node_handle.subscribe("camera", 100, &KinematicHandler::cameraCallback, &kinematic);
-	//ros::Subscriber kinematic_sub = node_handle.subscribe("/MATLAB/kinematic_publisher", 100, matlabCallback);
 
+	// Setup subscriber to get the computed angles by MATLAB
 	ros::Subscriber kinematics = node_handle.subscribe("/MATLAB/kinematic_publisher", 100, &KinematicHandler::kinematicCallback, &kinematic);
 
 	while(ros::ok())
 	{
 		ros::spinOnce();
-
 	}
 }
