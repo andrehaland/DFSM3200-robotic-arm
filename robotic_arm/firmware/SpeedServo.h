f27be653e6a4a2fca08ea7a79c3e3252068d6fc3
andrehaland <andre.haland@gmail.com>
2018-03-30 16:52:48 +0200

    Comments and cleanup

--- robotic_arm/firmware/SpeedServo.h
+++ robotic_arm/firmware/SpeedServo.h
@@ -9,6 +9,11 @@
 
 
 
+/**
+ * This class is used to better control the speed of the servo motors 
+ * Each object remembers the last written angle, and uses this variable
+ * to slowly increment the write-value
+ **/
 class SpeedServo
 {
 
@@ -19,6 +24,9 @@ public:
      **/
 	SpeedServo(Servo* servo_ptr, ros::NodeHandle* nh_ptr, const char* topic);
 
+    /**
+     * Default destructor
+     **/
 	~SpeedServo(){}
 
     /**
@@ -42,37 +50,79 @@ public:
      **/
     int desired() const { return m_desired; }
 
+    /**
+     * Set the desired angle 
+     **/
     void setDesired(int desired);
 
+    /**
+     * Returns the value of the counter 
+     **/
     unsigned int counter() const { return m_counter; }
 
+    /**
+     * Returns a bool telling whether the motor is moving
+     * in a positive direction, meaning next angle > current angle 
+     **/
     bool positive() const { return m_positive; }
 
+    /**
+     * Check if motor is in desired position. If not, it should slowly
+     * move towards the desired position until it reaches it 
+     **/
     void update();
 
+    /**
+     * Increment counter 
+     **/
     void increment();
 
+    /**
+     * Reset the counter
+     **/
     void resetCounter();
 
+    /**
+     * Callback function listening to the joint topic
+     * Used the recieved data to set desired angle
+     **/
     void servoCallback(const std_msgs::Float64& angle);
 
 private:
 
+    /**
+     * Pointer to Servo object
+     **/
 	Servo* m_servo;	
 
+    /**
+     * Pointer to NodeHandle used to subscribe to topics 
+     **/
     ros::NodeHandle* m_nodeHandlePtr;
 
+    /**
+     * Subscribers to joint angles 
+     **/
     ros::Subscriber<std_msgs::Float64, SpeedServo> m_jointSubscriber;
 
+    /**
+     * The last written joint angle 
+     **/
     int m_current;
 
+    /**
+     * The desired joint angle 
+     **/
     int m_desired;
 
+    /**
+     * What directions the joint should move 
+     **/
     bool m_positive;
 
+    /**
+     * Counter used to adjust speed of the joint movement 
+     **/
     unsigned int m_counter;
-
-
-
 };
 #endif
